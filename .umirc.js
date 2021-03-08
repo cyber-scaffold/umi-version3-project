85747c99bffcac9ab03ce9f04ab5f8f234931ce2
yushicheng <1542874601@qq.com>
2021-03-08 16:59:48 +0800

    temp:临时节点标记 星期一 下午 4:59:47 三月 8日 2021 创建了 config/proxy.js 修改了 .umirc.js

--- .umirc.js
+++ .umirc.js
@@ -1,10 +1,12 @@
-import { name } from "./package.json";
 import { defineConfig } from "umi";
-import routes from "./config/routes";
 
+import proxy from "./config/proxy";
+import routes from "./config/routes";
+import { name } from "./package.json";
 
 // ref: https://umijs.org/config/
 export default defineConfig({
+  proxy,
   routes,
   nodeModulesTransform:{type:"none"},
   devServer: {
